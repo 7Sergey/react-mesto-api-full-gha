364ae6846b98c11ebfdbd9f3a5dbc3aef84e6818
Sergey Fedorov <sergey14fedorov@gmail.com>
2023-12-05 22:59:07 +0300

    finish

--- backend/models/Card.js
+++ backend/models/Card.js
@@ -18,7 +18,7 @@ const cardSchema = new mongoose.Schema(
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: 'user',
       required: true,
     },
     likes: {
