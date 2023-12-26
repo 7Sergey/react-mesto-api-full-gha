3d570ed096bff00a964f066c75b53ce00f1f17d0
Sergey Fedorov <sergey14fedorov@gmail.com>
2023-12-26 13:05:02 +0300

    fix invalid token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,8 +3,6 @@ const jwt = require('jsonwebtoken') // импортируем модуль jsonw
 require('dotenv').config() // Подключаем переменные окружения из файла .env
 
 const { JWT_SECRET, NODE_ENV } = process.env
-console.log('JWT_SECRET:', JWT_SECRET)
-console.log('NODE_ENV:', NODE_ENV)
 
 const UnauthorizedError = require('../errors/unauthorized')
 
@@ -13,18 +11,15 @@ function auth(req, res, next) {
   try {
     const token = req.cookies.userToken
 
-    console.log('token:', token)
     if (!token) {
       throw new UnauthorizedError('NotAuthenticate')
     }
     const validToken = token.replace('Bearer ', '')
-    console.log('validToken:', validToken)
     // Добавлена проверка наличия строки "Bearer "
     payload = jwt.verify(
       validToken,
       NODE_ENV !== 'production' ? JWT_SECRET : 'dev_secret',
     )
-    console.log('payload:', payload)
     // Присваиваем айди пользователя для удаления/добавления лайков
     req.user = payload
     next()
